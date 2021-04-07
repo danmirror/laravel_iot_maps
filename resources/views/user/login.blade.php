fe00dc1924e54b273cdb9c46aba714d392dfcd24
danmirror <danuandreanid@gmail.com>
2021-04-08 03:55:02 +0700

    fix bug

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -2,24 +2,28 @@
 @section('title','Login')
 @section('content')
   <div class="container">
-    @if ($errors->any())
-      <div class="alert alert-danger">
-          <ul>
-              @foreach ($errors->all() as $error)
-                  <li>{{ $error }}</li>
-              @endforeach
-          </ul>
-      </div>
-    @endif
-    @if(session('alert'))
-      <div class="alert alert-danger" role="alert">
-      {{ session('alert') }}
-      </div>
-    @endif
+    
+ 
       <div id="container">
         <div class="boxed">
+          @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+          @endif
+          @if(session('alert'))
+            <div class="alert alert-danger" role="alert">
+            {{ session('alert') }}
+            </div>
+          @endif
+
           <form action="{{route('loginStore')}}" method="post">
           @csrf
+
             <div class="form-group">
               <label for="exampleInputEmail1">Email</label>
               <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
